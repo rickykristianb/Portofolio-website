c1d03a0df7159a24b69122c7b0d21f623f424807
Ricky Kristian B <ricky.kristianb@gmail.com>
2023-06-27 17:12:52 +0700

    resolve project page and start adding flask to the main file

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
+import os
+
+app = Flask(__name__)
+key = os.urandom(20)
+app.secret_key = key
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
